b39ee5d8ab5110cf96ffa8bb6292ed356f71cf6e
Maksim Anishchenko <maksimych60@gmail.com>
2022-10-18 18:54:04 +0300

    fall platform activity

--- Assets/Scripts/Game/Objects/FallPlatform.cs
+++ Assets/Scripts/Game/Objects/FallPlatform.cs
@@ -1,9 +1,47 @@
-﻿using UnityEngine;
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
 
 namespace P3D.Game
 {
     public class FallPlatform : MonoBehaviour
     {
-        
+        [SerializeField] private StandArea _standArea;
+        [SerializeField] private float _fallDelay;
+        [SerializeField] private float _fallDuration;
+        [SerializeField] private Ease _ease;
+
+        private Vector3 _startPosition;
+        private void Awake()
+        {
+            _startPosition = transform.position;
+        }
+
+        private void OnEnable()
+        {
+            _standArea.OnEntered += Fall;
+        }
+
+        private void Fall()
+        {
+            _standArea.OnEntered -= Fall;
+
+            Sequence sequence = DOTween.Sequence();
+            
+            sequence.AppendInterval(_fallDelay);
+            sequence.Append(transform
+                .DOMove(transform.position + new Vector3(0, -50, 0), _fallDuration)
+                .SetEase(_ease));
+
+            sequence.OnComplete(Reset);
+            
+            sequence.SetUpdate(UpdateType.Fixed);
+        }
+
+        private void Reset()
+        {
+            _standArea.OnEntered += Fall;
+            transform.position = _startPosition;
+        }
     }
 }
